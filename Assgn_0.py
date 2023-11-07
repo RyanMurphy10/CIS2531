94dad510fd46aeb78ba1a7e5a0e02ab9a236542f
Ryan Murphy <98363721+RyanMurphy10@users.noreply.github.com>
2023-11-07 15:52:17 -0500

    Add files via upload

---
+++ Assgn_0.py
@@ -0,0 +1,15 @@
+print("Welcome to my program.")
+FullName = input("Please enter your full name: ")
+
+print("\nHello, " + FullName)
+print("This program will ask you to enter the base and the height of a triangle. The program will calculate and print the area of the triangle")
+
+TriangleBase = float(input("\nEnter base of triangle: "))
+TriangleHeight = float(input("Enter height of triangle: "))
+
+TriangleArea = (((TriangleBase) * (TriangleHeight)) / 2)
+
+print(f'The area is: {TriangleArea}')
+
+print("\nThank you and have a nice day.")
+print("Good bye!")
